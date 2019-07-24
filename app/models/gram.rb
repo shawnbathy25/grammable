2646959f03eabe72af62de09380fd8d984f4fb52
Oluwaseun Oyogho <shawnbathy25@example.com>
2019-07-24 19:07:18 +0000

    style the app

--- app/models/gram.rb
+++ app/models/gram.rb
@@ -1,9 +1,11 @@
 class Gram < ApplicationRecord
 	validates :message, presence: true
+	validates :picture, presence: true
+	has_many :comments
 
 
 	mount_uploader :picture, PictureUploader
 
-	belongs_to :gram
+	belongs_to :user
 end
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -8,6 +8,10 @@ FactoryBot.define do
   end
    factory :gram do
     message { "hello" }
+    picture { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'picture.png').to_s, 'image/png') }
+
+
+
     association :user
   end
 end
